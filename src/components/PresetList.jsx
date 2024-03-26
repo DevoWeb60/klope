924e09e66a2792e6d13ddd93a702216bb19544ca
Thibault Berthelin <thibault.berthelin@outlook.fr>
2024-03-26 19:10:12 +0100

    Fix float number + aromes data json + add total

--- src/components/PresetList.jsx
+++ src/components/PresetList.jsx
@@ -3,41 +3,15 @@ import { List, ListItem, ListItemButton, Sheet, Typography, ListItemContent, Lis
 import WaterDropIcon from '@mui/icons-material/WaterDrop';
 import CalculateIcon from '@mui/icons-material/Calculate';
 import OpenInNewIcon from '@mui/icons-material/OpenInNew';
+import AROMES from "../data/aromes.json"
 
 export default function PresetList({ contentMax, aromeMax, calc }) {
-    const aromes = [
-        {
-            name: "Ragnarock",
-            brand: "A&L",
-            pourcent: 5,
-            link: "https://www.taklope.com/a-l/10766-concentre-ragnarok-sweet-edition.html?"
-        },
-        {
-            name: "Heisenberg",
-            brand: "Vampire Vape",
-            pourcent: 15,
-            link: "https://www.taklope.com/vampire-vape/1399-arome-heisenberg-vv.html"
-        },
-        {
-            name: "Pastèque Melon",
-            brand: "Punk Funk Hero",
-            pourcent: 8,
-            link: "https://www.taklope.com/punk-funk-hero/15693-concentre-pasteque-melon-30ml.html"
-        },
-        {
-            name: "Pinkman",
-            brand: "Vampire Vape",
-            pourcent: 15,
-            link: "https://www.taklope.com/vampire-vape/1407-arome-pinkman-vv.html"
-        }
-    ]
-
     return (
         <Sheet variant="soft" color="success" sx={{ my: 3, padding: 2, borderRadius: 10 }}>
             <Typography level="h4" startDecorator={<WaterDropIcon color="success" />} marginBottom={2}>Arômes</Typography>
             <List>
                 {
-                    aromes.map((arome, index) => (
+                    AROMES.map((arome, index) => (
                         <ListItem key={index} endAction={
                             <>
                                 <IconButton size="md" color="neutral" href={arome.link} component="a" target="_blank">
--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -2,13 +2,20 @@ import React from 'react'
 import { Typography, Grid } from "@mui/joy";
 
 export default function Result({ result }) {
+    const base = Math.round(result.content)
+    const arome = Math.round(result.arome)
+    const total = base + arome
+
     return (
         <Grid container spacing={2} flexDirection="column" sx={{ my: 3 }}>
             <Grid xs="6">
-                <Typography >Base : <Typography variant="outlined" color="primary">{result.content} ml</Typography></Typography>
+                <Typography >Base : <Typography variant="outlined" color="primary">{base} ml</Typography></Typography>
             </Grid>
             <Grid xs="6">
-                <Typography >Arôme : <Typography variant="outlined" color="primary">{result.arome} ml</Typography></Typography>
+                <Typography >Arôme : <Typography variant="outlined" color="primary">{arome} ml</Typography></Typography>
+            </Grid>
+            <Grid xs="6">
+                <Typography color="primary" fontWeight={"bold"}>Total : <Typography>{total} ml</Typography></Typography>
             </Grid>
         </Grid >
     )
